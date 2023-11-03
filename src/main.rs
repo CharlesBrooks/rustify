386301f04e21a16d174ae5c356162617daae1d7e
Charles Brooks <cebrooks93@gmail.com>
2023-11-02 23:41:10 -0400

    proper dep inclusion, super basic clap setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::{Parser};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    name: Option<String>,
+}
+
 fn main() {
+    let _cli = Cli::parse();
     println!("Hello, world!");
 }
